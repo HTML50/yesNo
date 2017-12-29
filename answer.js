4a939b2cb8dfcace9dcbab898d990865f2055fb1
HTML50 <iambenf@gmail.com>
2017-12-30 01:11:39 +0800

    upload

--- answer.js
+++ answer.js
@@ -1,16 +1,18 @@
   (function(){
-  var answer,video,
-  halfOfTotalVids = 5,
-  lucky = Math.floor(Math.random()*10) + 1;
-  if(lucky > halfOfTotalVids){
+  var answer,video,lucky,
+  random = Math.floor(Math.random()*11);
+  if(random > 5){
     answer = 'yes';
-    lucky -= halfOfTotalVids;
-  }else{
+    lucky = Math.floor(Math.random()*16);
+  }else if(random< 5){
     answer = 'no';
+    lucky = Math.floor(Math.random()*33);
+  }else{
+    answer = 'maybe';
+    lucky = 1;
   }
-  //video = 'https://html50.github.io/yesNo/assets/'+answer+'/'+lucky+'.mp4';
+  video = 'https://html50.github.io/yesNo/assets/'+answer+'/'+lucky+'.mp4';
   //video = 'http://127.0.0.1/yesNo/assets/'+answer+'/'+lucky+'.mp4';
-  video = 'http://127.0.0.1/yesNo/assets/no/1.mp4';
 
   $answer = {
     answer: answer,
